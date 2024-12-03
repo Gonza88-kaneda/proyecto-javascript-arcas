4035bd046b41eb03c19e7bf0e1f62b4930885502
Gonzalo Arcas <gonzaloezequielarcas88@gmail.com>
2024-12-03 20:21:41 -0300

    primer-commit de prueba

---
+++ main.js
@@ -0,0 +1,27 @@
+// variable es un espacio de memoria reservado para guardar informacion importante del programa
+
+//declaracion
+let cliente;
+
+//asignacion de vqalores
+cliente="carlos";
+
+console.log (cliente);
+
+//inicializar variables
+
+let edad = 27;
+
+console.log (edad);
+
+//const: es uan variable cuyo valor no puede ser modificado una vez que ha sido asignado.
+
+const nacimiento = 1987;
+
+console.log (nacimiento);
+
+const titulo = document.getElementById ("titulo");
+
+titulo.textContent = "hola mundo";
+titulo.style.color = "red";
+titulo.style.fontSize = "94px";
